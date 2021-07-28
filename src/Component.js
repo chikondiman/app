d7eeca5a050c0112abbe10179e96e3de30e77bb5
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-07-28 13:51:23 -0400

    syntax

--- src/Component.js
+++ src/Component.js
@@ -1,14 +1,17 @@
-1:  import React, { useState } from 'react';
-2:
-3:  function Example() {
-4:    const [count, setCount] = useState(0);
-5:
-6:    return (
-7:      <div>
-8:        <p>You clicked {count} times</p>
-9:        <button onClick={() => setCount(count + 1)}>
-10:         Click me
-11:        </button>
-12:      </div>
-13:    );
-14:  }
+import React, { useState } from 'react';
+
+function Component() {
+    const [count, setCount] = useState(0);
+
+    return (
+     <div>
+        <p>You clicked {count} times</p>
+       <button onClick={() => setCount(count + 1)}>
+        Click me
+        </button>
+      </div>
+   );
+  }
+
+  
+  export default Component 
