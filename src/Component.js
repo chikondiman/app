ee297e17a5fcfd0457082984979346f94097e894
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-15 17:10:42 -0400

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 function Component() {
     const [snack1, setCount1] = useState(0);
     const [snack2, setCount2] = useState(0);
-   const [votesAvailable, setVotesAvailable] = useState(3);
+   // const [votesAvailable, setVotesAvailable] = useState(3);
     return (
      <div>
         <p> Votes Left: {votesAvailable} </p>
         <p> Votes: {snack1} </p>
-       <button onClick={() => setCount1(snack1 + 1), setVotesAvailable(votesAvailable - 1)}>
+       {/* <button onClick={() => setCount1(snack1 + 1), setVotesAvailable(votesAvailable - 1)}>
         Vote here 
-        </button>
+        </button> */}
 
         <p>Votes: {snack2} </p>
        <button onClick={() => setCount2(snack2 + 1)}>
