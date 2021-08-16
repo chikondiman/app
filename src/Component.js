8aaeda67096f160fb9e883e4315b572d21822893
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-16 18:06:13 -0400

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 function Component() {
     const [snack1, setCount1] = useState(0);
     const [snack2, setCount2] = useState(0);
-    const [snack1, snack2];
+    const snacks = [snack1, snack2];
+
    const [votesAvailable, setVotesAvailable] = useState(3);
     return (
      <div>
