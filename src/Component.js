c2fbaeed24cef761951ff97b45a36810c4093133
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-20 14:46:55 -0400

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -10,7 +10,7 @@ function Component() {
  
     return (
      <div>
-        <p> Votes Remaining: {votesAvailable} </p>
+        <p> Votes : {votesAvailable} </p>
         <p> Votes: {snack1} </p>
        <button onClick={() => {
          subtractVote(votesAvailable - 1);
